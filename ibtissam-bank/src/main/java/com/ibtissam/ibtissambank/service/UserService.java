4057f4203ed534ca606f9cd3645f604f116ee93b
attoba <140215889+attoba@users.noreply.github.com>
2024-06-06 15:59:34 +0100

    Add files via upload

---
+++ ibtissam-bank/src/main/java/com/ibtissam/ibtissambank/service/UserService.java
@@ -0,0 +1,22 @@
+package com.ibtissam.ibtissambank.service;
+
+import com.ibtissam.ibtissambank.dto.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+
+public interface UserService {
+    BankResponse createAccount(UserRequest userRequest);
+    BankResponse login(LoginDto loginDto);
+
+    BankResponse loginEmploye(LoginDto loginDto);
+    BankResponse verify(CodeRequest codeRequest);
+
+
+    BankResponse getBalance(Authentication authentication);
+
+    BankResponse creditAccount(CreditDebitRequest request);
+    BankResponse debitAccount(CreditDebitRequest request);
+    BankResponse transfer(TransferRequest request);
+
+    BankResponse logout(String tokenHeader);
+}
